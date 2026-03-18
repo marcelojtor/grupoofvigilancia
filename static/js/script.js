44b6bb4c4a0c36c744709af2ff2b9c18df8af6ef
marcelojtor <marcelo.jtorres1975@gmail.com>
2026-03-18 10:57:10 -0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -47,17 +47,3 @@ document.querySelectorAll('.section').forEach(section => {
 
 });
 
-// ANIMAÇÃO IMAGENS SEGMENTOS
-const imagens = document.querySelectorAll('.detalhe-box img');
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-        }
-    });
-});
-
-imagens.forEach(img => {
-    observer.observe(img);
-});
